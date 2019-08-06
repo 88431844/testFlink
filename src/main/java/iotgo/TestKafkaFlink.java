ed853bdf6c2b73af868ae16fc3226112c9df59e0
gaozhihua <gaozhihua@xiaobangtouzi.com>
2019-08-06 18:07:44 +0800

    Initial commit

---
+++ src/main/java/iotgo/TestKafkaFlink.java
@@ -0,0 +1,42 @@
+package iotgo;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
+import org.apache.flink.streaming.api.operators.StreamingRuntimeContext;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
+import org.apache.flink.streaming.connectors.kafka.config.OffsetCommitMode;
+import org.apache.flink.streaming.connectors.kafka.internals.AbstractFetcher;
+import org.apache.flink.streaming.connectors.kafka.internals.AbstractPartitionDiscoverer;
+import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicPartition;
+import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicsDescriptor;
+import org.apache.flink.util.SerializedValue;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+
+public class TestKafkaFlink {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("zookeeper.connect", "localhost:2181");
+        props.put("group.id", "metric-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");  //key 反序列化
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "latest"); //value 反序列化
+
+//        DataStreamSource<String> dataStreamSource = see.addSource(new )
+//
+//        dataStreamSource.print(); //把从 kafka 读取到的数据打印在控制台
+//
+//        env.execute("Flink add data source");
+    }
+}
