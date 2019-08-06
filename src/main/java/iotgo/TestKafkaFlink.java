123fe181023b4868c956ade9e17a226d6d0a9910
gaozhihua <gaozhihua@xiaobangtouzi.com>
2019-08-06 21:13:54 +0800

    add flink for kafka test

--- src/main/java/iotgo/TestKafkaFlink.java
+++ src/main/java/iotgo/TestKafkaFlink.java
@@ -1,42 +1,38 @@
 package iotgo;
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.metrics.MetricGroup;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
-import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
-import org.apache.flink.streaming.api.operators.StreamingRuntimeContext;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
-import org.apache.flink.streaming.connectors.kafka.config.OffsetCommitMode;
-import org.apache.flink.streaming.connectors.kafka.internals.AbstractFetcher;
-import org.apache.flink.streaming.connectors.kafka.internals.AbstractPartitionDiscoverer;
-import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicPartition;
-import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicsDescriptor;
-import org.apache.flink.util.SerializedValue;
-
-import java.util.Collection;
-import java.util.Map;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import java.util.Properties;
 
 public class TestKafkaFlink {
 
     public static void main(String[] args) {
-        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("zookeeper.connect", "localhost:2181");
-        props.put("group.id", "metric-group");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");  //key 反序列化
+        props.put("bootstrap.servers", "ckafka.xiaobangtouzi.com:9092");
+        props.put("zookeeper.connect", "ckafka.xiaobangtouzi.com:2181");
+        props.put("group.id", "test-flink201908062111");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("auto.offset.reset", "latest"); //value 反序列化
+        props.put("auto.offset.reset", "latest");
+//        props.put("auto.offset.reset", "earliest");
 
-//        DataStreamSource<String> dataStreamSource = see.addSource(new )
-//
-//        dataStreamSource.print(); //把从 kafka 读取到的数据打印在控制台
-//
-//        env.execute("Flink add data source");
+        SingleOutputStreamOperator<String> kafka = env.addSource(new FlinkKafkaConsumer010<>(
+                "event-stream-qa",   //这个 kafka topic 需要和上面的工具类的 topic 一致
+                new SimpleStringSchema(),
+                props)).setParallelism(1)
+                .map(s -> "log:" + s);
+
+//        student.addSink(new SinkToMySQL()); //数据 sink 到 mysql
+        kafka.print();
+
+        try {
+            env.execute("test flink kafka");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
