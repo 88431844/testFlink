efa0db41bc13f41562b6ba29333518c100000e5f
miracle <88431844@163.com>
2019-08-19 20:30:48 +0800

    增加mysqlBinlog监控数据库DDL操作

---
+++ src/main/java/iotgo/MySqlBinlogWatcher.java
@@ -0,0 +1,60 @@
+package iotgo;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import iotgo.bean.MysqlBinlogInfo;
+import iotgo.util.CommonUtil;
+import iotgo.util.Const;
+import iotgo.util.HttpUtil;
+import iotgo.util.KafkaUtil;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.HashSet;
+
+public class MySqlBinlogWatcher {
+
+    private final static String mysqlBinlogTopic = "mysql-220";
+    private final static String kafkaGroupId = "mysqlBinlogWatcher_groupId_001";
+    private final static String flinkJobName = "mysqlBinlogWatcher";
+    //数据团队机器人
+    private final static String wxRobotUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=1b8cd3fc-3778-4c22-a865-15fab45f6816";
+    //测试机器人
+//    private final static String wxRobotUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=dc9adabc-def3-4bf3-9662-c619a57bad81";
+
+
+    public static void main(String[] args) {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        HashSet<String> mysqlDDLfilter = new HashSet<>();
+        mysqlDDLfilter.add("table-create");
+        mysqlDDLfilter.add("table-alter");
+        mysqlDDLfilter.add("table-drop");
+//        mysqlDDLfilter.add("update");
+
+        KafkaUtil.getKafkaByFlink(
+                mysqlBinlogTopic,
+                env,
+                KafkaUtil.getKafkaProperties(kafkaGroupId),
+                Const.FLINK_PARALLELISM,
+                CommonUtil.getArgsTimeStamp(args))
+                //Json to bean
+                .map(m -> JSON.parseObject(m, MysqlBinlogInfo.class))
+                //过滤出目标事件
+                .filter(m -> mysqlDDLfilter.contains(m.getType()))
+                //请求HTTP 企业微信机器人
+                .map(m -> HttpUtil.doPost(wxRobotUrl, getReqJson(m)));
+        try {
+            env.execute(flinkJobName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JSONObject getReqJson(MysqlBinlogInfo m) {
+        String content = JSON.toJSONString(m)
+                .replace("\"", "")
+                .replace("\\", "");
+        return JSON.parseObject("{\"msgtype\":\"text\",\"text\":{\"content\":\"" + content + "\"}}");
+    }
+}
---
+++ src/main/java/iotgo/bean/MysqlBinlogInfo.java
@@ -0,0 +1,20 @@
+package iotgo.bean;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MysqlBinlogInfo {
+
+    private String database;
+    private String table;
+    private String type;
+    private long ts;
+    private long xid;
+    private boolean commit;
+    private String data;
+
+}
